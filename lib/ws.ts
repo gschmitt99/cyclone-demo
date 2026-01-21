b8711de20c2b2466ffd33ef9257ad67eab85d7a7
gschmitt99@gmail.com <gschmitt99@gmail.com>
2026-01-21 13:33:59 -0600

    one additional hook

--- lib/ws.ts
+++ lib/ws.ts
@@ -1,9 +1,9 @@
 import { WebSocketServer } from "ws";
 import type WebSocket from "ws";
 
-if (!global._wss) {
-     global._wss = new WebSocketServer({ port: 3002, host: "0.0.0.0" });
-}
+// if (!global._wss) {
+//      global._wss = new WebSocketServer({ port: 3002, host: "0.0.0.0" });
+// }
 
 export function broadcast(type: any, payload: any = {}) {
     return; // don't do this on vercel at all.
